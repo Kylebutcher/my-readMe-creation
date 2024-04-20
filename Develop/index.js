1ffbad19db4181c7d1ff110ff5a319bbd0128052
kyle butcher <kylembutcher97@gmail.com>
2024-04-20 15:04:58 -0500

    have a boiler plate set up just about. may need to switch some questions around. i just set up some as place holders for now.

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,39 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer.prompt([
+  {
+    type: "input",
+    message: "Please enter some fun information about yourself!",
+    name: "information"
+  },
+  { type: "input",
+    message: "How old are you?",
+    name: "age"
+  },
+  {
+    type: "input",
+    message: "Where are you from?",
+    name: "location"
+  },
+  {
+    type: "input",
+    message: "Enter your Github Link."
+  }
+
+])
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  if(err) {
+    return console.log(err);
+  }
+    console.log('readMe is ready to view')
+}
 
 // TODO: Create a function to initialize app
 function init() {}
